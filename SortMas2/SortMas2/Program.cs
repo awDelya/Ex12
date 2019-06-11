13e4acc4926bb5e23beedae70c36163f705f32f9
Овчинникова Аделина <deley@mail.ru>
2019-06-11 19:29:15 +0500

    Add function BucketSort + Update function ChooseMas

--- SortMas2/SortMas2/Program.cs
+++ SortMas2/SortMas2/Program.cs
@@ -26,6 +26,7 @@ namespace SortMas2
         }
         private static int ChooseSort()
         {
+            Console.Clear();
             Color.Print("\n Выберите пункт меню", ConsoleColor.Yellow);
             Color.Print("\n\n 1) Блочная сортировка" +
                         "\n\n 2) Быстрая сортировка" +
@@ -99,6 +100,67 @@ namespace SortMas2
                 Color.Print("\n Колличество сравнений: " + ubvSrv + ", Колличество перестановок: " + ubvPer + "\n", ConsoleColor.Yellow);
             }
         }
+        static void BucketSort(int[] a, ref int sravnenie, ref int perestonovka)
+        {
+            // Примем, что количество корзин равно количеству элементов в массиве-источнике.
+            // Тогда:
+            // массив корзин
+            List<int>[] aux = new List<int>[a.Length];
+
+            // каждую корзину проинициализировать
+            for (int i = 0; i < aux.Length; ++i)
+                aux[i] = new List<int>();
+
+            // найти диапазон значений в массиве-источнике
+            int minValue = a[0];
+            int maxValue = a[0];
+
+            for (int i = 1; i < a.Length; ++i)
+            {
+                if (a[i] < minValue)
+                {
+                    minValue = a[i];
+                    sravnenie++;
+                }
+                else if (a[i] > maxValue)
+                {
+                    maxValue = a[i];
+                    sravnenie++;
+                }
+            }
+
+            // эта величина будет использоваться a.Length раз, поэтому имеет смысл её сохранить.
+            double numRange = maxValue - minValue;
+
+            for (int i = 0; i < a.Length; ++i)
+            {
+                // вычисление индекса корзины
+                int bcktIdx = (int)Math.Floor((a[i] - minValue) / numRange * (aux.Length - 1));
+
+                // добавление элемента в соответствующую корзину
+                aux[bcktIdx].Add(a[i]);
+                perestonovka++;
+            }
+
+            // сортировка корзин. Здесь я, для упрощения себе писанины, использую библиотечную сортировку
+            for (int i = 0; i < aux.Length; ++i)
+            {
+                aux[i].Sort();
+                sravnenie++;
+                perestonovka++;
+            }
+            // собираем отсортированные элементы обратно в массив-источник
+            int idx = 0;
+
+            for (int i = 0; i < aux.Length; ++i)
+            {
+                for (int j = 0; j < aux[i].Count; ++j)
+                {
+                    a[idx++] = aux[i][j];
+                    perestonovka++;
+                }
+            }
+        }
         static void Main()
         {
             bool ok = false;
@@ -129,7 +191,15 @@ namespace SortMas2
                             switch(ChooseSort())
                             {
                                 case 1:
-
+                                    rndPer = 0;
+                                    rndSrv = 0;
+                                    BucketSort(rndMas, ref rndSrv, ref rndPer);
+                                    vozPer = 0;
+                                    vozSrv = 0;
+                                    BucketSort(vozMas, ref vozSrv, ref vozPer);
+                                    ubvPer = 0;
+                                    ubvSrv = 0;
+                                    BucketSort(ubvMas, ref ubvSrv, ref ubvPer);
                                     break;
                                 case 2:
 
